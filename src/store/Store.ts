926377e859e21d21ca24c23af6ef76858f08e123
David Zhao <david.zhao.blog@gmail.com>
2022-04-14 14:56:08 -0400

    npm install reduxjs-toolkit-persist

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,13 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'reduxjs-toolkit-persist'
+import storage from 'reduxjs-toolkit-persist/lib/storage' // defaults to localStorage for web
+
 import logger from 'redux-logger'
 import { reducers } from './CombinedReducers'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
 export type RootState = ReturnType<typeof reducers>
 const store = configureStore({
-  reducer: reducers,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .concat(logger),
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+        // Ignore these action types, Alert and whenever showAlert is called.
+        ignoredActions: [
+            FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER,
+        ],
+        // // Ignore these field paths in all actions
+        // ignoredActionPaths: ['app.alert.buttons[0].handler'],
+        //   // Ignore these paths in the state
+        //   ignoredPaths: ['items.dates']
+        // persist/PERSIST is from 'redux-persist/integration/react'
+    }
+})
 })
 
 export type AppDispatch = typeof store.dispatch
